ac16dd1d6dc80e956023d0001605688c43bb24d0
wade-cheng <wadecheng3301@gmail.com>
2025-05-27 23:16:23 -0700

    add parsing Commands

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,16 @@
-use std::io::{self, Write};
+use std::{
+    io::{self, Write},
+    process::exit,
+};
+
+enum Command {
+    Exit,
+    Jobs,
+    Bg,
+    Fg,
+    Noop,
+    NonBuiltin(String),
+}
 
 pub struct App {
     /// The prompt for the shell.
@@ -25,12 +37,43 @@ impl App {
             match io::stdin().read_line(&mut input_buffer) {
                 Ok(0) => return, // exit on EOF (CTRL-D)
                 Ok(_) => {
-                    println!("got {}", input_buffer);
+                    Self::eval(&input_buffer);
                 }
                 Err(_) => panic!(),
             }
+
+            input_buffer.clear();
         }
 
         unreachable!();
+    }
+
+    fn eval(input: &str) {
+        let command = Self::parse(input);
+        match command {
+            Command::Exit => exit(0),
+            Command::Jobs => println!("Jobsing"),
+            Command::Bg => println!("Bging"),
+            Command::Fg => println!("Fging"),
+            Command::Noop => {}
+            Command::NonBuiltin(s) => println!("NonBuiltining: {s}"),
+        }
+    }
+
+    /// Parses a command line input into a `Command`.
+    ///
+    /// Note that we match for builtins on the first word.
+    /// This means `fg`, `fg sidjf`, and `fg --help` will return `Command::Fg`,
+    /// but `fg___` will not.
+    fn parse(input: &str) -> Command {
+        let first_word = input.split_whitespace().next();
+        match first_word {
+            Some("fg") => Command::Fg,
+            Some("bg") => Command::Bg,
+            Some("jobs") => Command::Jobs,
+            Some("exit") => Command::Exit,
+            Some(_) => Command::NonBuiltin(input.to_string()),
+            None => Command::Noop,
+        }
     }
 }
